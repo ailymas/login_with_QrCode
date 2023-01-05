bff00c5379eb2f4c38b1a85260cd936f402d5fbd
baggi <lyesbabas0@gmail.com>
2023-01-05 22:39:09 +0100

    add token authentication systeme

--- Backend/users/urls.py
+++ Backend/users/urls.py
@@ -1,7 +1,9 @@
-
-
-
+from rest_framework.authtoken.views import ObtainAuthToken
+from django.urls import path
+from . import views 
 
 urlpatterns = [
-
+    path("login/",ObtainAuthToken.as_view(), name="login"),
+    path("register/", views.UserCreateView.as_view(), name="register"),
+    path("me/", views.UserMeView.as_view(), name="me"),
 ]
