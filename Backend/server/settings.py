417509e9f90de9b927e939c562a7521a1cfa362f
baggi <lyesbabas0@gmail.com>
2023-01-05 15:19:43 +0100

    configure app users

--- Backend/server/settings.py
+++ Backend/server/settings.py
@@ -75,15 +75,10 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': "postgres",
-        "PASSWORD": "postgres",
-        "PORT": "5433"
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-AUTH_USER_MODEL = 'users.User'
 
 
 # Password validation
